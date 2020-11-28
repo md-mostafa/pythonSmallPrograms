35f9cf02149ce8697ae0fe37b05293f2aba4fcd6
md-mostafa <md.mostafa.akash@gmail.com>
2020-11-28 17:12:41 +0600

    500msg with pyautogui

---
+++ 500msg.py
@@ -0,0 +1,7 @@
+import pyautogui
+import time
+for i in range(50):
+    time.sleep(4)
+    pyautogui.typewrite('dost exam kokhon dibi?tor jonno bshe achi')
+    time.sleep(2)
+    pyautogui.press('enter')
